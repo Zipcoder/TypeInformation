db65748470028102015d831bb8218ca50462aac8
Aaron Long <aaron.long.c@gmail.com>
2017-05-02 16:36:48 -0400

    classImplementsInterface finished

--- src/main/java/io/github/aaronclong/TypeInformation/partOne/ClassExplorer.java
+++ src/main/java/io/github/aaronclong/TypeInformation/partOne/ClassExplorer.java
@@ -3,5 +3,29 @@ package io.github.aaronclong.TypeInformation.partOne;
 /**
  * Created by aaronlong on 5/2/17.
  */
-public class ClassExplorer {
+public class ClassExplorer<T> {
+    public ClassExplorer() {}
+
+    public boolean classImplementsInterface(Object compareClass, Class<T> theInterface) {
+        Class implementor = compareClass.getClass();
+        return theInterface.isAssignableFrom(implementor);
+    }
+
+    public boolean classImplementsInterface(String compareClass, Class<T> theInterface) {
+        try {
+            Class implementor = Class.forName(compareClass);
+            return theInterface.isAssignableFrom(implementor);
+
+        } catch(ClassNotFoundException e) {
+            System.out.println(e);
+        }
+
+        return false;
+    }
+
+    public boolean classImplementsInterface(Class compareClass, Class<T> theInterface) {
+        return theInterface.isAssignableFrom(compareClass);
+    }
+
+
 }
--- src/test/java/io/github/aaronclong/TypeInformation/partOne/ClassExplorerTest.java
+++ src/test/java/io/github/aaronclong/TypeInformation/partOne/ClassExplorerTest.java
@@ -1,7 +1,39 @@
 package io.github.aaronclong.TypeInformation.partOne;
 
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+
+import java.util.ArrayList;
+
 /**
  * Created by aaronlong on 5/2/17.
  */
 public class ClassExplorerTest {
+
+    private Class theInterface;
+    private ClassExplorer checkingClass;
+    private Class classInstance;
+
+    @Before
+    public void setUp() {
+        try {
+            theInterface = Class.forName("java.util.List");
+            checkingClass = new ClassExplorer();
+            classInstance = Class.forName("java.util.ArrayList");
+        } catch(ClassNotFoundException e) {
+            System.out.println(e);
+        }
+    }
+
+    @Test
+    public void testClassImplementsInterface() {
+        Assert.assertTrue("Checking Implementation with String passed",
+                checkingClass.classImplementsInterface("java.util.ArrayList", theInterface));
+        Object arrayListObject = new ArrayList<Integer>();
+        Assert.assertTrue("Checking Implementation with Object passed",
+                checkingClass.classImplementsInterface(arrayListObject, theInterface));
+        Assert.assertTrue("Checking Implementation with Class object passed",
+                checkingClass.classImplementsInterface(classInstance, theInterface));
+    }
 }
