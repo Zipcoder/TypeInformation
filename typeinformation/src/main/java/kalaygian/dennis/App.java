5108b5e817f01c3ab34f26bb5bd9e1f5e74f2459
Dennis Kalaygian <denniskalaygian@zipcoder12.local>
2017-05-03 09:53:30 -0400

    Set up maven archetype

---
+++ typeinformation/src/main/java/kalaygian/dennis/App.java
@@ -0,0 +1,13 @@
+package kalaygian.dennis;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
