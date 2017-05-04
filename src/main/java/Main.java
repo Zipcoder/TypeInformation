b3898c1df22825844e1d4482e684fa0bfd022bd7
Andrew Wong <wong.andrew.p@gmail.com>
2017-05-04 19:44:19 -0400

    Better single resp

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -5,6 +5,8 @@ public class Main {
     public static void main(String[] args) {
         PartOne partOne = new PartOne();
         StringBuilder sb = new StringBuilder();
-        System.out.println(partOne.listAllMembers(ExampleClass.class));
+//        System.out.println(partOne.listAllMembers(ExampleClass.class));
+        ExampleClass object = new ExampleClass();
+        System.out.println(partOne.listAllMembers(object));
     }
 }
--- src/main/java/PartOne.java
+++ src/main/java/PartOne.java
@@ -8,10 +8,8 @@ import java.lang.reflect.Modifier;
  * Created by andrewwong on 5/4/17.
  */
 public class PartOne {
-    String currentModifier;
-    String currentMember;
-    String newLine = "\n";
-    String space = " ";
+    private final String newLine = "\n";
+    private final String space = " ";
     public boolean classImplementsInterface(Class<?> aClass, String interfaceName) {
         boolean isImplemented = false;
         Class[] interfaces = aClass.getInterfaces();
@@ -23,58 +21,59 @@ public class PartOne {
         }
         return isImplemented;
     }
-    public String listAllMembers(Class object){
-        StringBuilder stringBuilder = new StringBuilder();
-        String list = "";
-        int currentModifierEncoding;
-        String currentModifier;
-        String currentMember;
-        String newLine = "\n";
-        String space = " ";
-        Field[] declaredFields = object.getDeclaredFields();
-        Method[] declaredMethods = object.getDeclaredMethods();
-        Constructor<?>[] declaredConstructors = object.getDeclaredConstructors();
-        for(Field field: declaredFields){
-            currentModifierEncoding = field.getModifiers();
-            currentModifier = Modifier.toString(currentModifierEncoding);
-            currentMember = field.getName();
-            stringBuilder.append(currentModifier);
-            stringBuilder.append(space);
-            stringBuilder.append(currentMember);
-            stringBuilder.append(newLine);
-        }
-        for(Method method: declaredMethods){
-            currentModifierEncoding = method.getModifiers();
-            currentModifier = Modifier.toString(currentModifierEncoding);
-            currentMember = method.getName();
-            stringBuilder.append(currentModifier);
-            stringBuilder.append(space);
-            stringBuilder.append(currentMember);
-            stringBuilder.append(newLine);
-        }
-        for(Constructor constructor: declaredConstructors){
-            currentModifierEncoding = constructor.getModifiers();
-            currentModifier = Modifier.toString(currentModifierEncoding);
-            currentMember = constructor.getName();
-            stringBuilder.append(currentModifier);
-            stringBuilder.append(space);
-            stringBuilder.append(currentMember);
-            stringBuilder.append(newLine);
-        }
-        list = stringBuilder.toString();
+    public String listAllMembers(Object o){
+        StringBuilder sb = new StringBuilder();
+        String list;
+        String fieldList;
+        String methodList;
+        String constructorList;
+        fieldList = listFields(o);
+        methodList = listMethods(o);
+        constructorList = listConstructors(o);
+        sb.append(fieldList);
+        sb.append(methodList);
+        sb.append(constructorList);
+        list = sb.toString();
         return list;
     }
 
-    public String listFields(Class object, StringBuilder sb){
-        Field[] fields = object.getFields();
+    private String listFields(Object o){
+        StringBuilder sb = new StringBuilder();
+        Field[] fields = o.getClass().getDeclaredFields();
         for(Field field: fields){
             sb.append(Modifier.toString(field.getModifiers()));
             sb.append(space);
             sb.append(field.getName());
             sb.append(newLine);
         }
-        String list = sb.toString();
-        return list;
+        return sb.toString();
     }
+
+    private String listMethods(Object o){
+        StringBuilder sb = new StringBuilder();
+
+        Method[] methods = o.getClass().getMethods();
+        for(Method method: methods){
+            sb.append(Modifier.toString(method.getModifiers()));
+            sb.append(space);
+            sb.append(method.getName());
+            sb.append(newLine);
+        }
+        return sb.toString();
+    }
+
+    private String listConstructors(Object o){
+        StringBuilder sb = new StringBuilder();
+
+        Constructor<?>[] constructors = o.getClass().getConstructors();
+        for(Constructor constructor: constructors){
+            sb.append(Modifier.toString(constructor.getModifiers()));
+            sb.append(space);
+            sb.append(constructor.getName());
+            sb.append(newLine);
+        }
+        return sb.toString();
+    }
+
 
 }
