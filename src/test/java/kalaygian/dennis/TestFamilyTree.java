e5ed31e67942c79bb677b43b79177e373a8544ab
Dennis Kalaygian <denniskalaygian@zipcoder12.local>
2017-05-05 17:25:09 -0400

    In the process of writing tests for instantiateClassHierarchy(); skipping listAllMembers() for the meantime in order to proceed with labs

--- src/test/java/kalaygian/dennis/TestFamilyTree.java
+++ src/test/java/kalaygian/dennis/TestFamilyTree.java
@@ -2,7 +2,11 @@ package kalaygian.dennis;
 
 import org.junit.Test;
 import org.junit.Assert;
+
+import java.util.AbstractCollection;
+import java.util.AbstractList;
 import java.util.ArrayList;
+import java.util.List;
 
 
 public class TestFamilyTree{
@@ -50,4 +54,56 @@ public class TestFamilyTree{
         //Then
         Assert.assertEquals("Method is not returning the right value", expectedValue, returnValue);
     }
+
+    /*@Test
+    public void testListAllMembersReturnType() throws ClassNotFoundException {
+        //Given
+        FamilyTree familyTree = new FamilyTree(Class.forName("java.lang.Boolean"));
+        Class expectedReturnType = String.class;
+        //When
+        Class returnType = familyTree.listAllMembers().getClass();
+        //Then
+        Assert.assertEquals("Method is returning the wrong data type", expectedReturnType, returnType);
+    }
+
+    @Test
+    public void testListAllMembers() throws ClassNotFoundException {
+        //Given
+        FamilyTree familyTree = new FamilyTree(Class.forName("java.lang.Boolean"));
+        String expectedValue = "java.lang.Object" + "\n" + "  " + "java.util.AbstractCollection" + "\n" + "    " + "java.util.AbstractList" + "\n";
+        //When
+        String returnValue = familyTree.getClassHierarchy();
+        //Then
+        Assert.assertEquals("Method is not returning the right value", expectedValue, returnValue);
+    }*/
+
+    @Test
+    public void testInstantiateClassHierarchyReturnType() throws ClassNotFoundException {
+        //Given
+        FamilyTree familyTree = new FamilyTree(Class.forName("java.util.AbstractList"));
+        Class expectedReturnType = String.class;
+        //When
+        Class returnType = familyTree.getClassHierarchy().getClass();
+        //Then
+        Assert.assertEquals("Method is returning the wrong data type", expectedReturnType, returnType);
+    }
+
+    @Test
+    public void testInstantiateClassHierarchy() throws ClassNotFoundException {
+        //Given
+        FamilyTree familyTree = new FamilyTree(Class.forName("java.util.AbstractList"));
+        ArrayList item1;
+        AbstractList item2;
+        AbstractCollection item3;
+        Object item4;
+        Object[] expectedList = new Object[4];
+        expectedList[0] = item1;
+        expectedList[1] = null;
+
+        //When
+        Object[]
+        //Then
+        Assert.assertEquals("Method is not returning the right value", expectedValue, returnValue);
+    }
+
 }
