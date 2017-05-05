31b0c0902a7cb39d059b0ab9c4ce9c98fe2c5b72
Chris <chriskim80@gmail.com>
2017-05-05 16:43:15 -0400

    Created mini-method stub and wrote test for getMethod(Class, String)

--- src/main/java/kim/christopher/unitcorn/UnitCornTestRunner.java
+++ src/main/java/kim/christopher/unitcorn/UnitCornTestRunner.java
@@ -1,5 +1,21 @@
 package kim.christopher.unitcorn;
 
+import org.junit.runner.Result;
+
+import java.lang.reflect.Method;
+
 public class UnitCornTestRunner {
+
+    public Result runTest(Class c, String methodName){
+        return new Result();
+    }
+
+    public Method getMethod(Class c, String methodName){
+        return Double.class.getMethods()[1];
+    }
+
+    public String runTests(Class c){
+        return "";
+    }
 
 }
--- src/test/java/kim/christopher/unitcorn/UnitCornTestRunnerTester.java
+++ src/test/java/kim/christopher/unitcorn/UnitCornTestRunnerTester.java
@@ -1,5 +1,58 @@
 package kim.christopher.unitcorn;
 
+import org.junit.Before;
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+import java.lang.reflect.Method;
+import java.util.ArrayList;
+import java.util.Arrays;
+
 public class UnitCornTestRunnerTester {
 
+    Class c;
+    String methodName;
+    UnitCornTestRunner runner;
+
+    @Before
+    public void initialize() {
+        c = Double.class;
+        methodName = "isNaN";
+        runner = new UnitCornTestRunner();
+    }
+
+    @Test
+    public void getMethodTest(){
+
+        //: When
+        Method result = runner.getMethod(c, methodName);
+        String expectedValue = "isNaN";
+        String actualValue = result.getName();
+
+        //: Then
+        assertEquals(expectedValue, actualValue);
+
+    }
+
+    @Test
+    public void runTestTest(){
+
+        //: Given
+
+        //: When
+
+        //: Then
+
+    }
+
+    @Test
+    public void runTestsTest(){
+
+        //: Given
+
+        //: When
+
+        //: Then
+
+    }
 }
